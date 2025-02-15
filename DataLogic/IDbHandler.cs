90188e7383a03c3b8404f572ecc1a7ba6ac9040e
SarvashAgnointel <sarvash@agnointel.ai>
2025-02-15 12:19:52 +0530

    recommit on 15-02-2025

---
+++ DataLogic/IDbHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DBAccess
+{
+    public interface IDbHandler
+    {
+        DataTable ExecuteDataTable(string query, Dictionary<string, object> parameters = null, CommandType commandType = CommandType.Text);
+        int ExecuteNonQuery(string query, Dictionary<string, object> parameters = null, CommandType commandType = CommandType.Text);
+        object ExecuteScalar(string query, Dictionary<string, object> parameters = null, CommandType commandType = CommandType.Text);
+    }
+}
